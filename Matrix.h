b18d13f97ea86ed1f4a281b0e8feddce689be81a
Asya02 <74601520+Asya02@users.noreply.github.com>
2022-03-15 15:54:16 +0300

    Add files via upload

--- Matrix.h
+++ Matrix.h
@@ -1,5 +1,25 @@
 #pragma once
 #include <iostream>
 
-template <typename T>
+template <typename T> // T - любой тип данных
 class Matrix
+{
+private:
+	int rows;
+	int columns;
+public:
+	Matrix();
+	Matrix(int r, int c);
+	~Matrix();
+
+	//gets
+	int getRows();
+	int getColumns();
+
+
+	//sets
+	void setRows(int r);
+	void setColumns(int c);
+
+
+};
