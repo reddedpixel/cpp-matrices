df936cf4027b44500bfb858b462ec441cd459681
reddedpixel <74682334+reddedpixel@users.noreply.github.com>
2022-03-22 16:46:05 +0300

    fixed ????????

--- Matrix.h
+++ Matrix.h
@@ -30,7 +30,9 @@ public:
 
     Matrix<T>()
     {
-
+        rows = 0;
+        columns = 0;
+        element = nullptr;
     }
     
 	Matrix<T>(int r, int c)
@@ -38,23 +40,30 @@ public:
         rows = r;
         columns = c;
         element = new T[r * c];
-        //for (int i = 0; i < rows; i++)
-        //{
-        //    element[i] = new T[c];
-        //}
     }
 
     ~Matrix<T>() { std::cout << "deleted" << std::endl; }
 
 	//gets
     int getRows() { return rows; }
-
     int getColumns() { return columns; }
 
 	//sets
     void setRows(int r) { rows = r; }
     void setColumns(int c) { columns = c; }
 
+    // Get element
+    Iterator operator[](int i)
+    {
+        if (i < 0 || i >= rows)
+        {
+            std::cerr << "Unable to find cell with index " << i << std::endl;
+            exit(0);
+        }
+        return element + i * rows;
+    }
+
+    // Print out matrix
 	void print()
     {
         for (int i = 0; i < rows; i++)
@@ -67,6 +76,7 @@ public:
         }
     }
 
+    // Fill in matrix
 	void fill()
     {
         for (int i = 0; i < rows; i++)
@@ -78,6 +88,7 @@ public:
         }
     }
 
+    // Check if two matrices are equal
 	bool operator==(Matrix<T> b)
     {
         if ((rows != b.rows) or (columns != b.columns))
@@ -93,28 +104,24 @@ public:
         return true;
     }
 
-	Iterator operator[](int i)
-    {
-        if (i < 0 || i >= rows)
-        {
-            std::cerr << "Unable to find cell with index " << i << std::endl;
-            exit(0);
-        }
-        return element + i * rows;
-    }
-
-	Matrix<T> operator* (int k)
-    {
-        Matrix<T> M(rows, columns);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                M.element[i * columns + j] *= /*element[i * rows + j] **/ k;
-            }
-        }
-        return M;
-    }
-
-	Matrix<T> operator* (Matrix<T> b)
+    // Multiply matrix by number
+     Matrix<T> operator* (int k)
+     {
+         Matrix<T> M(rows, columns);
+         mCopy(this, M);
+         for (int i = 0; i < rows; i++) 
+         {
+             for (int j = 0; j < columns; j++) 
+             {
+                 for (int l = 0; l < k; l++)
+                    M.element[i * columns + j] += M.element[i * columns + j];
+             }
+         }
+         return M;
+     }
+    
+    // Multiply two matrices
+    Matrix<T> operator* (Matrix<T> b)
     {
         if (columns != b.rows)
         {
@@ -135,6 +142,7 @@ public:
         return M;
     }
 
+    // Add matrices together
     Matrix<T> operator+(Matrix<T> b)
     {
         if ((columns != b.columns) || (rows != b.rows))
